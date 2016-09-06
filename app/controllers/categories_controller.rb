98cc5a437e4140db9769e90efdaf124d48f37b94
Giovanni e Edson <g10v45+github@gmail.com>
2016-09-05 21:50:57 -0300

    create category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,24 @@ class CategoriesController < ApplicationController
     @jobs = @category.jobs.all
   end
 
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category
+    else
+      flash[:error] = 'Preencha o nome da categoria'
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+
+  end
 end
